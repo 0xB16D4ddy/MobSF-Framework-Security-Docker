53f728ababa4e8a2ea038ed8da5a75879b508ee1
0xB1GD4ddy <phuocem201@gmail.com>
2024-12-07 20:26:50 +0700

    feat: add some script to run mobsf temporary

---
+++ run.ps1
@@ -0,0 +1,3 @@
+docker pull opensecurity/mobile-security-framework-mobsf:latest
+docker run -it --rm -p 8484:8000 opensecurity/mobile-security-framework-mobsf:latest
+
---
+++ run.sh
@@ -0,0 +1,2 @@
+docker pull opensecurity/mobile-security-framework-mobsf:latest
+docker run -it --rm -p 8484:8000 opensecurity/mobile-security-framework-mobsf:latest
